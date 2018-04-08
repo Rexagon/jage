48a586e41d36fbd76478b14f0817896762232d00
Ivan <reide740@gmail.com>
2018-04-08 20:01:02 +0300

    Combined Transformable and GameObject classes

--- jage/GameObject.cpp
+++ jage/GameObject.cpp
@@ -1,13 +1,25 @@
 #include "GameObject.h"
 
+#ifndef GLM_ENABLE_EXPERIMENTAL
+#define GLM_ENABLE_EXPERIMENTAL
+#endif
+
 #include <stack>
+
+#include <glm/gtx/matrix_decompose.hpp>
 
 #include "Log.h"
 
-
 GameObject::GameObject(EntityManager * manager, EntityId id) :
 	m_isActive(true), m_parent(nullptr), 
-	m_manager(manager), m_id(id), m_isPendingDestroy(false)
+	m_manager(manager), m_id(id), m_isPendingDestroy(false),
+	m_position(0.0f, 0.0f, 0.0f), m_rotation(1.0f, 0.0f, 0.0f, 0.0f), m_scale(1.0f, 1.0f, 1.0f),
+	m_directionFront(0.0f, 0.0f, -1.0f), m_directionRight(1.0f, 0.0f, 0.0f), m_directionUp(0.0f, 1.0f, 0.0f),
+	m_transformation(1.0f),
+	m_positionMatrix(1.0f), m_positionMatrixInversed(1.0f),
+	m_rotationMatrix(1.0f), m_rotationMatrixInversed(1.0f),
+	m_scaleMatrix(1.0f), m_scaleMatrixInversed(1.0f),
+	m_positionChanged(true), m_rotationChanged(true), m_scaleChanged(true), m_transformationChanged(true)
 {
 }
 
@@ -44,16 +56,6 @@ void GameObject::setTag(const std::string & tag)
 std::string GameObject::getTag() const
 {
 	return m_tag;
-}
-
-mat4 GameObject::getGlobalTransformation() const
-{
-	if (m_parent == nullptr) {
-		return getTransformationMatrix();
-	}
-	else {
-		return m_parent->getGlobalTransformation() * getTransformationMatrix();
-	}
 }
 
 const EntityManager * GameObject::getEntityManager() const
@@ -227,4 +229,267 @@ bool GameObject::operator<(const GameObject & other) const
 std::shared_ptr<GameObject> GameObject::create(EntityManager * manager, EntityId id)
 {
 	return std::make_shared<GameObject>(manager, id);
+}
+
+mat4 GameObject::getGlobalTransformation() const
+{
+	if (m_parent == nullptr) {
+		return getTransformationMatrix();
+	}
+	else {
+		return m_parent->getGlobalTransformation() * getTransformationMatrix();
+	}
+}
+
+void GameObject::setTransformationMatrix(const mat4 & transform)
+{
+	glm::vec3 skew;
+	glm::vec4 perspective;
+
+	glm::decompose(transform, m_scale, m_rotation, m_position, skew, perspective);
+}
+
+mat4 GameObject::getTransformationMatrix() const
+{
+	updatePosition();
+	updateRotation();
+	updateScale();
+
+	if (m_transformationChanged) {
+		m_transformation = getPositionMatrix() * getRotationMatrix() * getScaleMatrix();
+		m_transformationChanged = false;
+	}
+
+	return m_transformation;
+}
+
+mat4 GameObject::getPositionMatrix() const
+{
+	updatePosition();
+
+	return m_positionMatrix;
+}
+
+mat4 GameObject::getPositionMatrixInversed() const
+{
+	updatePosition();
+
+	return m_positionMatrixInversed;
+}
+
+mat4 GameObject::getRotationMatrix() const
+{
+	updateRotation();
+
+	return m_rotationMatrix;
+}
+
+mat4 GameObject::getRotationMatrixInversed() const
+{
+	updateRotation();
+
+	return m_rotationMatrixInversed;
+}
+
+mat4 GameObject::getScaleMatrix() const
+{
+	updateScale();
+
+	return m_scaleMatrix;
+}
+
+mat4 GameObject::getScaleMatrixInversed() const
+{
+	updateScale();
+
+	return m_scaleMatrixInversed;
+}
+
+void GameObject::move(float x, float y, float z)
+{
+	m_position.x += x;
+	m_position.y += y;
+	m_position.z += z;
+	m_positionChanged = true;
+}
+
+void GameObject::move(const vec3 & vector)
+{
+	m_position += vector;
+	m_positionChanged = true;
+}
+
+void GameObject::setPosition(float x, float y, float z)
+{
+	m_position = vec3(x, y, z);
+	m_positionChanged = true;
+}
+
+void GameObject::setPosition(const vec3 & position)
+{
+	m_position = position;
+	m_positionChanged = true;
+}
+
+vec3 GameObject::getPosition() const
+{
+	return m_position;
+}
+
+void GameObject::rotate(float x, float y, float z)
+{
+	m_rotation = quat(vec3(x, y, z)) * m_rotation;
+	m_rotation = glm::normalize(m_rotation);
+	m_rotationChanged = true;
+}
+
+void GameObject::rotate(const vec3 & eulerAngles)
+{
+	m_rotation = quat(eulerAngles) * m_rotation;
+	m_rotation = glm::normalize(m_rotation);
+	m_rotationChanged = true;
+}
+
+void GameObject::rotate(const quat & rotation)
+{
+	m_rotation = rotation * m_rotation;
+	m_rotation = glm::normalize(m_rotation);
+	m_rotationChanged = true;
+}
+
+void GameObject::setRotation(float x, float y, float z)
+{
+	m_rotation = quat(vec3(x, y, z));
+	m_rotation = glm::normalize(m_rotation);
+	m_rotationChanged = true;
+}
+
+void GameObject::setRotation(const vec3 & eulerAngles)
+{
+	m_rotation = quat(eulerAngles);
+	m_rotation = glm::normalize(m_rotation);
+	m_rotationChanged = true;
+}
+
+void GameObject::setRotation(const quat & rotation)
+{
+	m_rotation = rotation;
+	m_rotation = glm::normalize(m_rotation);
+	m_rotationChanged = true;
+}
+
+quat GameObject::getRotation() const
+{
+	return m_rotation;
+}
+
+vec3 GameObject::getEulerRotation() const
+{
+	return glm::eulerAngles(m_rotation);
+}
+
+void GameObject::scale(float s)
+{
+	m_scale *= s;
+	m_scaleChanged = true;
+}
+
+void GameObject::scale(float x, float y, float z)
+{
+	m_scale *= vec3(x, y, z);
+	m_scaleChanged = true;
+}
+
+void GameObject::scale(const vec3 & s)
+{
+	m_scale *= s;
+	m_scaleChanged = true;
+}
+
+void GameObject::setScale(float s)
+{
+	m_scale = vec3(s, s, s);
+	m_scaleChanged = true;
+}
+
+void GameObject::setScale(float x, float y, float z)
+{
+	m_scale = vec3(x, y, z);
+	m_scaleChanged = true;
+}
+
+void GameObject::setScale(const vec3 & s)
+{
+	m_scale = s;
+	m_scaleChanged = true;
+}
+
+vec3 GameObject::getScale() const
+{
+	return m_scale;
+}
+
+vec3 GameObject::getDirectionFront() const
+{
+	updateRotation();
+
+	return m_directionFront;
+}
+
+vec3 GameObject::getDirectionRight() const
+{
+	updateRotation();
+
+	return m_directionRight;
+}
+
+vec3 GameObject::getDirectionUp() const
+{
+	updateRotation();
+
+	return m_directionUp;
+}
+
+void GameObject::updatePosition() const
+{
+	if (m_positionChanged) {
+		m_positionMatrix = glm::translate(mat4(1.0f), m_position);
+		m_positionMatrixInversed = glm::translate(mat4(1.0f), -m_position);
+
+		m_positionChanged = false;
+		m_transformationChanged = true;
+	}
+}
+
+void GameObject::updateRotation() const
+{
+	if (m_rotationChanged) {
+		m_rotationMatrix = glm::mat4_cast(m_rotation);
+		m_rotationMatrixInversed = glm::inverse(m_rotationMatrix);
+
+		// update direction also
+		vec4 temp(0.0f, 0.0f, -1.0f, 1.0f);
+		temp = m_rotationMatrix * temp;
+		temp /= temp.w;
+		vec3 directionFront(temp.x, temp.y, temp.z);
+
+		m_directionFront = glm::normalize(directionFront);
+		m_directionRight = glm::cross(m_directionFront, vec3(0.0f, 1.0f, 0.0f));
+		m_directionUp = glm::cross(m_directionRight, m_directionFront);
+
+		m_rotationChanged = false;
+		m_transformationChanged = true;
+	}
+}
+
+void GameObject::updateScale() const
+{
+	if (m_scaleChanged) {
+		m_scaleMatrix = glm::scale(mat4(1.0f), m_scale);
+		m_scaleMatrixInversed = glm::scale(mat4(1.0f),
+			vec3(1.0f / m_scale.x, 1.0f / m_scale.y, 1.0f / m_scale.z));
+
+		m_scaleChanged = false;
+		m_transformationChanged = true;
+	}
 }
--- jage/GameObject.h
+++ jage/GameObject.h
@@ -7,9 +7,9 @@
 #include <map>
 
 #include "EntityManager.h"
-#include "Transformable.h"
+#include "Math.h"
 
-class GameObject : public Transformable
+class GameObject
 {
 public:
 	GameObject(EntityManager* manager, EntityId id);
@@ -24,7 +24,6 @@ public:
 	void setTag(const std::string& tag);
 	std::string getTag() const;
 
-	mat4 getGlobalTransformation() const;
 
 	const EntityManager* getEntityManager() const;
 
@@ -127,6 +126,49 @@ public:
 	bool operator!=(const GameObject& other) const;
 	bool operator<(const GameObject& other) const;
 
+
+	// Transformation functions
+	mat4 getGlobalTransformation() const;
+
+	void setTransformationMatrix(const mat4& transformation);
+	mat4 getTransformationMatrix() const;
+
+	mat4 getPositionMatrix() const;
+	mat4 getPositionMatrixInversed() const;
+
+	mat4 getRotationMatrix() const;
+	mat4 getRotationMatrixInversed() const;
+
+	mat4 getScaleMatrix() const;
+	mat4 getScaleMatrixInversed() const;
+
+	void move(float x, float y, float z);
+	void move(const vec3& vector);
+	void setPosition(float x, float y, float z);
+	void setPosition(const vec3& position);
+	vec3 getPosition() const;
+
+	void rotate(float x, float y, float z);
+	void rotate(const vec3& eulerAngles);
+	void rotate(const quat& rotation);
+	void setRotation(float x, float y, float z);
+	void setRotation(const vec3& eulerAngles);
+	void setRotation(const quat& rotation);
+	quat getRotation() const;
+	vec3 getEulerRotation() const;
+
+	void scale(float s);
+	void scale(float x, float y, float z);
+	void scale(const vec3& s);
+	void setScale(float s);
+	void setScale(float x, float y, float z);
+	void setScale(const vec3& s);
+	vec3 getScale() const;
+
+	vec3 getDirectionFront() const;
+	vec3 getDirectionRight() const;
+	vec3 getDirectionUp() const;
+
 protected:
 	std::string m_name;
 	std::string m_tag;
@@ -139,10 +181,39 @@ protected:
 private:
 	friend class EntityManager;
 
+	void updatePosition() const;
+	void updateRotation() const;
+	void updateScale() const;
+
 	static std::shared_ptr<GameObject> create(EntityManager* manager, EntityId id);
 
 	EntityManager* m_manager;
 	EntityId m_id;
 
 	bool m_isPendingDestroy;
+
+	vec3 m_position;
+	quat m_rotation;
+	vec3 m_scale;
+
+	mutable vec3 m_directionFront;
+	mutable vec3 m_directionRight;
+	mutable vec3 m_directionUp;
+
+	mutable mat4 m_transformation;
+
+	mutable mat4 m_positionMatrix;
+	mutable mat4 m_positionMatrixInversed;
+
+	mutable mat4 m_rotationMatrix;
+	mutable mat4 m_rotationMatrixInversed;
+
+	mutable mat4 m_scaleMatrix;
+	mutable mat4 m_scaleMatrixInversed;
+
+	mutable bool m_positionChanged;
+	mutable bool m_rotationChanged;
+	mutable bool m_scaleChanged;
+
+	mutable bool m_transformationChanged;
 };
